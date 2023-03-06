9bea4691cde846aa0e9d10b80d31b81a3c8dda24
malbsubo28 <amsbox182@gmail.com>
2023-03-06 17:18:24 +0700

    update 06 Mar 17:18

--- praktikum2_fungsi_modul_python/percobaan1.py
+++
@@ -1,3 +0,0 @@
-from percobaan1_module import fungsi_nilai_huruf
-
-print(fungsi_nilai_huruf(3))
--- praktikum2_fungsi_modul_python/percobaan3.py
+++
@@ -1,5 +0,0 @@
-from percobaan3_module import hitung_bilangan
-
-bilangan = input("Masukkan data pisahkan dengan ',' : ")
-
-hitung_bilangan(bilangan)
